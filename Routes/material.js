2d153be5a0ad995405011fe8a8ca9619656f942c
ubaidi5 <ubaid7625@gmail.com>
2024-06-08 02:41:26 -0700

    change in file path in multer

--- Routes/material.js
+++ Routes/material.js
@@ -5,9 +5,7 @@ const mime = require('mime-types');
 const fs = require('fs');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../uploads/'));
-  },
+  destination:"uploads/",
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     const originalName = file.originalname.toLowerCase().split(' ').join('-');
