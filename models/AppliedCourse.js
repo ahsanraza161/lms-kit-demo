b33820dce01ac5dfa619a535b7722e97bd51955f
ahsanraza161 <ahsanrazainfo.com>
2024-07-02 14:45:40 +0500

    changes in server crashed problem

--- models/AppliedCourse.js
+++ models/AppliedCourse.js
@@ -1,23 +1,23 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const AppliedCourseSchema = new mongoose.Schema({
-//   usertype: String,
-//   branch: String,
-//   name: String,
-//   fatherName: String,
-//   dateOfBirth: String,
-//   gender: String,
-//   cnic: String,
-//   address: String,
-//   qualification: String,
-//   subject: String,
-//   completionYear: Number,
-//   universityCollege: String,
-//   email: {
-//     type: String,
-//     unique: true,
-//   },
-// });
+const AppliedCourseSchema = new mongoose.Schema({
+  usertype: String,
+  branch: String,
+  name: String,
+  fatherName: String,
+  dateOfBirth: String,
+  gender: String,
+  cnic: String,
+  address: String,
+  qualification: String,
+  subject: String,
+  completionYear: Number,
+  universityCollege: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+});
 
-// const AppliedCourse = mongoose.model('AppliedCourse', AppliedCourseSchema);
-// module.exports = AppliedCourse;
+const AppliedCourse = mongoose.model('AppliedCourse', AppliedCourseSchema);
+module.exports = AppliedCourse;
